a001c714f4d2215fc77b9a78702168258348891f
Debashishcodes <debashishs969@gmail.com>
2026-02-09 22:03:26 +0530

    Day40

---
+++ SORTING/BUBBLE SORT/sortStringDec.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s = "AZYZXBDJKX";
+    string str="";
+    for(int i=0;i<s.size();i++){
+        if(s[i]>='X'){
+            str.push_back(s[i]);
+        }
+    }
+    sort(str.begin(),str.end());
+    cout<<str;
+}
+//sort a string in decreasing order of values associated after removal of values smaller than x 
---
+++ SORTING/BUBBLE SORT/sorting.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[]={5,4,3,2,1};
+    int n =5;
+    for(int i =0;i<=n-1;i++){
+        cout<<arr[i]<<" ";
+    }
+    for(int i=0;i<=n-1;i++){
+        bool flag = false ;
+        for(int j=0;j<=n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag = false;
+            }
+        }
+        if(flag==true) {
+        break;
+        }
+    }
+    cout<<endl;
+    for(int i =0;i<=n-1;i++){
+        cout<<arr[i]<<" ";
+    }
+}
