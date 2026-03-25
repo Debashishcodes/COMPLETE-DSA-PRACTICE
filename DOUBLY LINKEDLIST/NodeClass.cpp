405bcd99ae9a2e4d21d096d1e06a815f6fadc89b
Debashishcodes <debashishs969@gmail.com>
2026-03-25 21:05:41 +0530

    Day81

---
+++ DOUBLY LINKEDLIST/NodeClass.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node * next;
+    Node * prev;
+    Node ( int v){
+        data = v;
+        next = NULL;
+        prev = NULL;
+    }
+};
+void display(Node *head){
+    Node * temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+
+    Node * a = new Node (10);
+    Node * b = new Node (20);
+    Node * c = new Node (30);
+    Node * d = new Node (40);
+    Node * e = new Node (50);
+
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+
+    e->prev = d;
+    d->prev = c;
+    c->prev = b;
+    b->prev = a;
+
+    display(a);
+
+}
