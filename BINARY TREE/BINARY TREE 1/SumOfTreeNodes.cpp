a767861725aa64ad50d95637873e3ad0118caf8f
Debashishcodes <debashishs969@gmail.com>
2026-05-21 21:26:34 +0530

    Day136

---
+++ BINARY TREE/BINARY TREE 1/SumOfTreeNodes.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node * left ;
+    Node * right;
+    Node(int data){
+        val = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+void displayTree(Node * root){
+    if(root == NULL) return;
+    cout<<root->val<<" ";
+    displayTree(root->left);
+    displayTree(root->right);
+}
+int sumOfNodes(Node * root){
+    if(root == NULL) return 0;
+    int ans = root->val + sumOfNodes(root->left) + sumOfNodes(root->right);
+    return ans;
+}
+int main(){
+    Node * a = new Node(1);
+    Node * b = new Node(2);
+    Node * c = new Node(3); 
+    Node * d = new Node(4);
+    Node * e = new Node(5);
+    Node * f = new Node(6);
+    Node * g = new Node(7);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    displayTree(a);
+    cout<<"\nSum of all nodes in the tree is : "<<sumOfNodes(a);
+    //cout<<a->val<<" "<<b->val<<" "<<c->val;
+}
